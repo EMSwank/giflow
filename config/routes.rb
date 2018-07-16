e7dd3faa29551d5c4c038350f9910c8335fc4ba0
Eliot Swank <eliotswank@gmail.com>
2018-07-15 18:46:14 -0600

    adds gifs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'gif/new'
+
+  get 'gif/index'
+
+  get 'gif/create'
+
+  get 'gif/destroy'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  resources :gifs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
