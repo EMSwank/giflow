d47bba911b7e8af3e7f3e8609dc4280b781cb32c
Eliot Swank <eliotswank@gmail.com>
2018-07-17 11:26:07 -0600

    adds user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716003447) do
+ActiveRecord::Schema.define(version: 20180717172230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gifs", force: :cascade do |t|
     t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
