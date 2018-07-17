f555b3d66eb93b1d1c8b8a1890e7c13e8e0e24f2
Eliot Swank <eliotswank@gmail.com>
2018-07-17 11:11:23 -0600

    adds form/buttons for login/new account

--- spec/features/static_pages/visitor_logs_in_spec.rb
+++ spec/features/static_pages/visitor_logs_in_spec.rb
@@ -7,11 +7,19 @@ describe "A Visitor" do
 
       click_on "Log in"
 
-      expect(current_path).to eq(login_path) 
+      expect(page).to have_content("Log into your account")
+      expect(current_path).to eq(login_path)
     end
 
-    it 'does something' do
-      
-    end
+  context "visits login page" do
+    it 'clicks on new account link and is taken to the new_user path' do
+      visit login_path
+
+      click_on "Sign up!"
+
+      expect(current_path).to eq(new_user_path)
+      end
+  end
+
   end
 end
