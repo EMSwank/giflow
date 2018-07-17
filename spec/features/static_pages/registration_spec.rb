c3af945be59f8cb8bc1f791718d833fe87bae5ae
Eliot Swank <eliotswank@gmail.com>
2018-07-17 11:49:04 -0600

    adds test for creating new user

--- spec/features/static_pages/registration_spec.rb
+++ spec/features/static_pages/registration_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 describe "registration workflow" do
   context "signs up for user account" do
     it "submits registration for successfully" do
-      
+      visit new_user_path
+      save_and_open_page
+
+      fill_in :name,	with: "name"
+      fill_in :email,	with: "email@email.com"
+      fill_in :password,	with: "password"
+
+      click_on :submit
+      expect(path).to eq(users_path)
     end
   end
 end
