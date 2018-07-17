e1b74ea445c3b66589d53490e0daa00f11c83b26
Eliot Swank <eliotswank@gmail.com>
2018-07-17 12:05:43 -0600

    adds password column to user model and routes and controller func

--- spec/features/static_pages/registration_spec.rb
+++ spec/features/static_pages/registration_spec.rb
@@ -4,13 +4,12 @@ describe "registration workflow" do
   context "signs up for user account" do
     it "submits registration for successfully" do
       visit new_user_path
-      save_and_open_page
 
-      fill_in :name,	with: "name"
-      fill_in :email,	with: "email@email.com"
-      fill_in :password,	with: "password"
+      fill_in :user_name,	with: "name"
+      fill_in :user_email,	with: "email@email.com"
+      fill_in :user_password,	with: "password"
 
-      click_on :submit
+      click_on 'Create User'
       expect(path).to eq(users_path)
     end
   end
