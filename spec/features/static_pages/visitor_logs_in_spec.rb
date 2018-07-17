d31d35e718813f797e5286145bacb38364e65852
Eliot Swank <eliotswank@gmail.com>
2018-07-17 10:55:15 -0600

    file cleanup/rename

--- spec/features/static_pages/visitor_logs_in_spec.rb
+++ spec/features/static_pages/visitor_logs_in_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 describe "A Visitor" do
   context "visits the root page" do
-    it "clicks on login link to login" do
+    it "clicks on login link to login and is take to the login page" do
       visit root_path
 
       click_on "Log in"
 
       expect(current_path).to eq(login_path) 
+    end
+
+    it 'does something' do
+      
     end
   end
 end
