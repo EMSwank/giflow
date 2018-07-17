02ade5fe361caaf60575eb8ed904cd525233cd0f
Eliot Swank <eliotswank@gmail.com>
2018-07-17 11:41:58 -0600

    user model creation and validations

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
 
   resources :gifs, only: [:new, :index, :create, :destroy]
-  resources :users, only: [:new]
+  resources :users, only: [:new, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/features/static_pages/registration_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe "registration workflow" do
+  context "signs up for user account" do
+    it "submits registration for successfully" do
+      
+    end
+  end
+end
