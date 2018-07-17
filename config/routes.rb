196fa6245612dab775f182d8cdd120d807c5a1d1
Eliot Swank <eliotswank@gmail.com>
2018-07-17 11:13:45 -0600

    adds routes and removes unneccessary files

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
 
   resources :gifs, only: [:new, :index, :create, :destroy]
-
+  resources :users, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
