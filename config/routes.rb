c47dd7b712c6cf08e79a318371b5b112f714271a
Eliot Swank <eliotswank@gmail.com>
2018-07-17 08:24:05 -0600

    adds login / logout paths to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
 
   resources :gifs, only: [:new, :index, :create, :destroy]
 
