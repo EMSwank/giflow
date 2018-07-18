5553ce97566ff57ad20e1f7797b5d8f591cbb86c
Eliot Swank <eliotswank@gmail.com>
2018-07-18 10:27:45 -0600

    adds admin exclusivity to admin dashboard controller

--- spec/features/admin/admin_sees_a_dashboard_spec.rb
+++ spec/features/admin/admin_sees_a_dashboard_spec.rb
@@ -11,4 +11,18 @@ describe "User visits dashboard page" do
       expect(page).to have_content("Admin Dashboard")
     end
   end
+
+  context "as a default user" do
+    it 'disallows default users to see the admin dashboard' do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_dashboards_path
+
+      expect(page).to_not have_content("Admin Dashboard")
+      expect(page).to have_content("The page you were looking for doesn't exist.") 
+    end
+  end
+  
 end
