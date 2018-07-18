29cb4419f0496471084c5ffd5f3089de0c3260f8
Eliot Swank <eliotswank@gmail.com>
2018-07-17 23:55:27 -0600

    completes user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
 
   def new
-    
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      # flash[:error] "Username/password combination is invalid"
+      render :new
+    end
   end
 end
